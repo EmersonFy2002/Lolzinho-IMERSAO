b2fd02ebbc93177c16b8c91245dcfc09fb49d865
EmersonFy2002 <ess8184871@gmail.com>
2024-09-06 22:28:56 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+        titulo : "Malzahar",
+        descricao : "Malzahar é um vidente fanático de League of Legends, devoto à chegada do Vazio. Com visões sombrias e poderes proféticos, ele busca a aniquilação do mundo, acreditando que a destruição é a única forma de purificação. Seguindo as vozes que ecoam em sua mente, Malzahar invoca criaturas abissais e inflige tormento aos seus inimigos, espalhando a corrupção do Vazio por Runeterra.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/malzahar/",
+        tags: " Mago vazio mid midlaner "
+    },
+    {
+        titulo : "Ryze",
+        descricao : "Ryze é um mago itinerante de League of Legends, conhecido por sua profunda conexão com a magia arcana. Com um passado marcado pela destruição causada por um poder descontrolado, ele dedica sua vida a proteger o mundo das Runas Globais, fragmentos de magia pura que podem tanto criar quanto destruir. Ryze é um mestre da manipulação de energia, capaz de conjurar poderosos feitiços e desferir ataques devastadores. Sua busca incessante por equilíbrio e conhecimento o torna um dos campeões mais enigmáticos e respeitados de Runeterra.",
+        link : "https://www-leagueoflegends-com.translate.goog/en-au/champions/ryze/?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc&_x_tr_hist=true",
+        tags: "mago humano mid midlaner "
+    },
+    {
+        titulo : "Galio",
+        descricao : "Galio é um colossal golem de pedra, guardião da cidade de Demacia. Criado para proteger sua gente, ele é uma força imponente no campo de batalha, capaz de aguentar grandes quantidades de dano e desferir golpes devastadores. Com um coração puro e uma devoção inabalável à justiça, Galio é um defensor incansável dos fracos e oprimidos.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/galio/",
+        tags: "mago tank mid midlaner colossu golem "
+    },
+    {
+        titulo : "Cassiopeia",
+        descricao : "Cassiopeia é uma sedutora e perigosa maga, mestre em manipular venenos e feitiços arcanos. Com uma beleza mortal e uma inteligência astuta, ela utiliza seus poderes para controlar seus inimigos e alcançar seus objetivos. Cassiopeia é uma força a ser temida no campo de batalha, capaz de transformar seus oponentes em pedra com um simples olhar.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/cassiopeia/",
+        tags: "mago mid midlaner cobra veneno botas "
+    },
+    {
+        titulo : "Jinx",
+        descricao : "Uma psicopata e gênio da criação de armas que adora causar caos em Piltover e Zaun.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/jinx/",
+        tags: "atirador adc adcarry humana maluca "
+    },
+    {
+        titulo : "Ahri",
+        descricao : "Uma vastaya sedutora com a capacidade de manipular as emoções dos outros.",
+        link :"https://www.leagueoflegends.com/pt-br/champions/ahri/",
+        tags: "vastaya mago mid midlaner raposa novecaudas"
+    },
+    {
+        titulo : "Zed",
+        descricao : "Um mestre da sombra e líder da Ordem das Sombras, buscando vingança contra os Noxianos.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/zed/",
+        tags: "mid midlaner assasino energia ninja shuriken"
+    },
+    {
+        titulo : "Lux",
+        descricao : "Uma maga poderosa de Demacia, que busca usar seus poderes para proteger seu reino.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/lux/",
+        tags: "mago mid midlaner luz huamana fada"
+    },
+    {
+        titulo : "Darius",
+        descricao : "Um noxiano brutal e implacável, conhecido por sua força e sede de sangue.",
+        link : "https://www.leagueoflegends.com/pt-br/champions/darius/",
+        tags: "homem top toplaner soldado bruto"
+    }
+];
+
